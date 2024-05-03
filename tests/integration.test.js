b59c77729ca3faef3bed3e28b1f585f376d30ea5
David Ventura <davidventura27@gmail.com>
2024-05-03 23:07:51 +0200

    add notes

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -7,11 +7,8 @@ import assert from "assert";
 
 const hstrToU8 = (hs) => new Uint8Array(hs.match(/../g).map((h) => parseInt(h, 16)));
 describe("integration", () => {
-  it("emits login event upon logging in", () => {
-    // TODO
-  });
-
   it("discovers a device", () => {
+    // LanSearch (server) -> PunchPkt (camera) -> discovered serial
     buildLogger("trace");
     const EXPECTED_SERIAL = "BATD156362WONJM";
     const punchPkt = "f14100144241544400000000000262ca574f4e4a4d000000";
@@ -29,5 +26,12 @@ describe("integration", () => {
       ev.emit("close");
       mockSock.close();
     });
+  });
+  // TODO
+  it("emits login event upon logging in", () => {
+    // LanSearch (server) -> PunchPkt (camera)
+    // vvv need to call makeSession vvv
+    // P2pRdy (server) -> P2pRdy (camera)
+    // Drw<Login> (server) -> [DrwAck, Drw<LoginAck>]
   });
 });
